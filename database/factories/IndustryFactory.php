ca3fca59f337d87bde653a3057ba13e78a12bbca
Daniel Berry <daniel@jstreetdigital.com>
2022-05-06 00:17:41 -0500

    forgot to update Industry factory and remove the seeder.
    Signed-off-by: Daniel Berry <daniel@jstreetdigital.com>

--- database/factories/IndustryFactory.php
+++ database/factories/IndustryFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Team;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use JetBrains\PhpStorm\ArrayShape;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Industry>
@@ -14,10 +17,12 @@ class IndustryFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition()
+    #[ArrayShape(['team_id' => "mixed", 'user_id' => "mixed", 'name' => "string"])] public function definition(): array
     {
         return [
-            //
+            'team_id' => Team::factory(),
+            'user_id' => User::factory(),
+            'name' => $this->faker->word
         ];
     }
 }
