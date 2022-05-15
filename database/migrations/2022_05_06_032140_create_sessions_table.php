8f72f1032acaa27e58c246363a114357aeb616e1
Daniel Berry <daniel@jstreetdigital.com>
2022-05-15 01:10:11 -0500

    primary key issue for sessions
    Signed-off-by: Daniel Berry <daniel@jstreetdigital.com>

--- database/migrations/2022_05_06_032140_create_sessions_table.php
+++ database/migrations/2022_05_06_032140_create_sessions_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
+        \Illuminate\Support\Facades\DB::statement('SET SESSION sql_require_primary_key=0');
+        
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
